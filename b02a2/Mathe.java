1158d9e6ddd03e36df22e60af5526ef50c900884
felixkhaw <felix.knupper@haw-hamburg.de>
2026-04-24 21:46:44 +0200

    Fertigstellen von Aufgabe 2a

--- b02a2/Mathe.java
+++ b02a2/Mathe.java
@@ -1,15 +1,22 @@
 public class Mathe {
     public static int fak(int n){
-        int tmp = 0;
-        while(n>0){
-            System.out.println("Runde: " + n);
-            if(tmp == 0){
-                tmp = n;
-            }
-            System.out.println("tmp: " + tmp + " = " + tmp + " * " + n +"\n");
-            tmp = tmp * (n-1);
-            --n;
+        int tmp;
+        // Den Fall abdecken das n eine negative Zahl ist.
+        if(n < 0){
+            tmp = n * (-1);
+        } else {
+            tmp = n;
         }
-        return tmp;   
+        
+	    int i = 1;
+        while(n > 1){
+            System.out.println("Runde: " + i);
+            // System.out.println("tmp: " + tmp + "*" + (n-1) + " = " +  tmp*(n-1) );
+            tmp = tmp * (n-1);
+		
+	    ++i;
+	    --n;
+        }
+	    return tmp;
     }
 }
--- b02a2/TestMathe.java
+++ b02a2/TestMathe.java
@@ -1,5 +1,5 @@
 public class TestMathe {
     public static void main(String[]args){
-        System.out.println(Mathe.fak(8));
+        System.out.println("Die Faktultät von 8 ist: " + Mathe.fak(8));
     }
 }
