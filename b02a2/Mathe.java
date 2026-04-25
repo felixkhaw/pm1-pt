27d4f314da7718effdb65729e094143d2a5938f0
felixkhaw <felix.knupper@haw-hamburg.de>
2026-04-25 11:46:22 +0200

    Verbesserung Abbruchbedingung

--- b02a2/Mathe.java
+++ b02a2/Mathe.java
@@ -1,17 +1,15 @@
 public class Mathe {
     public static int fakLoop(int n){
         int tmp;
-        // Den Fall abdecken das n eine negative Zahl ist.
         if(n < 0){
-            tmp = n * (-1);
+            System.out.println("Error: es wurde eine negative Zahl übergeben!!!")
+            return;
         } else {
             tmp = n;
         }
         
 	    int i = 1;
         while(n > 1){
-            // System.out.println("Runde: " + i);
-            // System.out.println("tmp: " + tmp + "*" + (n-1) + " = " +  tmp*(n-1) );
             tmp = tmp * (n-1);
 		
 	    ++i;
