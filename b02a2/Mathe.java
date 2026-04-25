5b6770b17b2782a8217c63d257b828efeb3004f8
felixkhaw <felix.knupper@haw-hamburg.de>
2026-04-25 23:41:06 +0200

    Korrigierte Tab Einstellungen von xed

--- b02a2/Mathe.java
+++ b02a2/Mathe.java
@@ -1,33 +1,30 @@
 public class Mathe {
-    public static int fakLoop(int n){
-        int tmp;
-        if(n < 0){
-            return -1;
-        } else {
-            tmp = n;
-        }
-        
-	    int i = 1;
-        while(n > 1){
-            tmp = tmp * (n-1);
-		
-	    ++i;
+	public static int fakLoop(int n){
+		int tmp;
+		if(n < 0){
+			return -1;
+		} else {
+			tmp = n;
+		}
+
+		while(n > 1){
+			tmp = tmp * (n-1);
 	    --n;
-        }
-	    return tmp;
-    }
+		}
+		return tmp;
+}
 
-    public static int fakRek(int n){
-        if(n == 0){ return 1; }
-        int fak = n * fakRek(n-1);
-        return fak;  
-    }
+	public static int fakRek(int n){
+		if(n == 0){ return 1; }
+		int fak = n * fakRek(n-1);
+		return fak;
+	}
 
-    public static void quadrateAusgabeRueck(int n){
-        while(n>0){
-            int quad = n*n;
-            System.out.println("Das Quadrat der Zahl " + n + " ist " + quad);
-            --n;
-        } 
-    }
+	public static void quadrateAusgabeRueck(int n){
+		while(n>0){
+			int quad = n*n;
+			System.out.println("Das Quadrat der Zahl " + n + " ist " + quad);
+			--n;
+		}
+	}
 }
--- b02a2/TestMathe.java
+++ b02a2/TestMathe.java
@@ -1,19 +1,19 @@
 public class TestMathe {
-    public static void main(String[]args){
-        if (args.length != 1) {
+	public static void main(String[]args){
+		if (args.length != 1) {
 			System.out.println("Verwendung java test: <zahl>");
 			return;
 		}
 
 		int fak = Integer.parseInt(args[0]);
-    
-        if(Mathe.fakLoop(fak) == -1){
-            System.out.println("Fehler, falsche Eingabe!");
-            return;
-        }
 
-        System.out.println("Die Faktultät von " + fak + " mit Math.fakLoop() ist: " + Mathe.fakLoop(fak));
-        System.out.println("Die Faktultät von " + fak + " mit Math.fakLoop() ist: " + Mathe.fakRek(fak));
-        Mathe.quadrateAusgabeRueck(fak);
-    }
+		if(Mathe.fakLoop(fak) == -1){
+			System.out.println("Fehler, falsche Eingabe!");
+			return;
+		}
+		
+		System.out.println("Die Faktultät von " + fak + " mit Math.fakLoop() ist: " + Mathe.fakLoop(fak));
+		System.out.println("Die Faktultät von " + fak + " mit Math.fakLoop() ist: " + Mathe.fakRek(fak));
+		Mathe.quadrateAusgabeRueck(fak);
+	}
 }
