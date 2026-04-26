1f12effc4a80cda78eeb27d54d087a107716b3b8
felixkhaw <felix.knupper@haw-hamburg.de>
2026-04-26 09:40:38 +0200

    Teste Funktionen in b02a3

--- b02a3/ZeichenketteTest.java
+++ b02a3/ZeichenketteTest.java
@@ -1,5 +1,9 @@
 public class ZeichenketteTest {
 	public static void main(String[] args){
+		System.out.println("Testen der Klasse Zeichenkette.java");
+		System.out.println("Aufruf der Methode grussMitZeit mit den Parametern 10 und 'J'");
+		//Teste Fehler bei der Methode grussMitZeit
+		System.out.println(Zeichenkette.grussMitZeit(10, "J"));
 		System.out.println(Zeichenkette.zitronenEis(5));
 	}
 }
