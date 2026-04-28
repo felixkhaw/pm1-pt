747edf54f1de9f6df8fd713de75970854109cc20
felixkhaw <felix.knupper@haw-hamburg.de>
2026-04-28 12:57:56 +0200

    b02a4

--- b02a4/TestZeichenkette.java
+++ b02a4/TestZeichenkette.java
@@ -1,5 +1,10 @@
 public class TestZeichenkette {
     public static void main(String[] args){
-        
-    }
+		System.out.println("Test der funktion wiederhole der Klasse Zeichenkette mit den Parametern: [1-20], Humba, ','");
+		int n = 1;
+		while(n<20){
+			System.out.println("("+ n +") " + Zeichenkette.wiederhole(n,"Humba", ','));
+			++n;			
+		}
+	}
 }
--- b02a4/Zeichenkette.java
+++ b02a4/Zeichenkette.java
@@ -1,4 +1,16 @@
 public class Zeichenkette {
 
-    public static String
+    public static String wiederhole(int n, String s, char t){
+		String tmp = "";
+		while(n>0){
+			if(n==1){
+				tmp += s;
+			} else {
+				tmp += s;
+				tmp += t;
+			}
+			--n;
+		}
+		return tmp;
+	}
 }
